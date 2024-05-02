95130e8fbe099b0f1fd5ab6cb43a4971c623e6f3
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-02 14:35:47 +0700

    create example for primitives

---
+++ example/the-primitives/index.ts
@@ -0,0 +1,9 @@
+const str: string = "thanh";
+
+const num: number = 12312323;
+
+const isExist: boolean = true;
+
+console.log(`this is my name: ${str}
+my password: ${num}
+account exist ${isExist}`);
