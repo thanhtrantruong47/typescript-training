f29413fb8145bbb375caead894425a4a62ad4842
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-02 14:52:43 +0700

    create example for array

---
+++ example/array/index.ts
@@ -0,0 +1,11 @@
+const arrNum: number[] = [1, 5, 4, 3, 2, 4, 5];
+
+const arrStr: string[] = ["thanh", "tran"];
+
+for(let i = 0 ; i < arrNum.length ; i++) {
+    console.log(arrNum[i])
+}
+
+for(let i = 0 ; i < arrStr.length ; i++) {
+    console.log(arrStr[i])
+}
