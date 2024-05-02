f5be321ee189d8f2772960e1f1167714631b4eb0
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-02 16:42:24 +0700

    update example for array

--- example/array/index.ts
+++ example/array/index.ts
@@ -2,10 +2,23 @@ const arrNum: number[] = [1, 5, 4, 3, 2, 4, 5];
 
 const arrStr: string[] = ["thanh", "tran"];
 
-for(let i = 0 ; i < arrNum.length ; i++) {
-    console.log(arrNum[i])
+
+// define array with arr[0] is string and arr[1] is number
+function doSomething(pair: [string, number]) {
+  const a = pair[0];
+
+  const b = pair[1];
+
+  console.log(a);
+  console.log(b);
 }
 
-for(let i = 0 ; i < arrStr.length ; i++) {
-    console.log(arrStr[i])
+doSomething(["hello", 42]);
+
+for (let i = 0; i < arrNum.length; i++) {
+  console.log(arrNum[i]);
+}
+
+for (let i = 0; i < arrStr.length; i++) {
+  console.log(arrStr[i]);
 }
