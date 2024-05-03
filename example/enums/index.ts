4191071bf3dad45c213d1c7fffaa418e29b63ab5
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-03 17:11:14 +0700

    add more example for enums

--- example/enums/index.ts
+++ example/enums/index.ts
@@ -5,8 +5,20 @@ enum Message {
 
 const login = (user: { email: string; password: string }) => {
   let ms = Message;
-  return user.email.length < 8  ? ms.EMAIL : ""
+  return user.email.length < 8 ? ms.EMAIL : "";
 };
 
+console.log(login({ email: "", password: "123123123123213" }));
 
-console.log(login({ email: "", password: "123123123123213" }))
+enum X {
+  A = 1,
+  B,
+  C,
+  D = 5,
+  F,
+  E,
+  G,
+  H,
+}
+
+console.log(typeof(X))
