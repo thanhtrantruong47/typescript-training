338a699794f719eef3e8ab6836ca7ceea7a584c8
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-10 10:22:26 +0700

    change file scripts

--- practice-typescript/src/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-import UserController from "./controllers/user"
-import UserView from "./views/user"
-
-document.addEventListener('DOMContentLoaded', () => {
-  const userController = new UserController(new UserView)
-  userController.handleAdd()
-})
