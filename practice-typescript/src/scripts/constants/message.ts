9587e477a98b15b39df1d445a3f37372acd76e1c
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-21 14:05:27 +0700

    delete field password in model and constant message

--- practice-typescript/src/scripts/constants/message.ts
+++ practice-typescript/src/scripts/constants/message.ts
@@ -1,6 +1,5 @@
 enum MESSAGE_ERROR {
   EMAIL = 'Please enter a valid email address',
-  PASSWORD = 'Password must be at least 7 characters long',
   FIRST_NAME = 'Please enter your first name',
   LAST_NAME = 'Please enter your last name',
   PHONE_NUMBER = 'Please enter your phone format 0-xxx-xxx-xxx',
