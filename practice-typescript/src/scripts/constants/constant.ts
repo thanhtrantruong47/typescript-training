7d27e3b94b0d826af8967e2fcfd25908e81694d6
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-22 10:44:17 +0700

    validate for special character and max length limit

--- practice-typescript/src/scripts/constants/constant.ts
+++ practice-typescript/src/scripts/constants/constant.ts
@@ -1,3 +1,5 @@
-const MAX_LENGTH = 20;
+const MAX_LENGTH = 12;
+const FIRST_NAME = 'Your first name ';
+const LAST_NAME = 'Your last name ';
 
-export { MAX_LENGTH };
+export { MAX_LENGTH, FIRST_NAME, LAST_NAME };
--- practice-typescript/src/scripts/constants/message.ts
+++ practice-typescript/src/scripts/constants/message.ts
@@ -1,7 +1,9 @@
 enum MESSAGE_ERROR {
   EMAIL = 'Please enter a valid email address',
-  FIRST_NAME = 'Please enter your first name with a maximum of 20 characters',
-  LAST_NAME = 'Please enter your last name with a maximum of 20 characters',
+  FIRST_NAME = 'Please enter your first name',
+  LAST_NAME = 'Please enter your last name',
+  SPECIAL_CHARACTER = 'should not include a special character',
+  GREATER_THAN_LENGTH = 'cannot exceed 12 characters',
   PHONE_NUMBER = 'Please enter your phone format 0-123-456-789',
   EMPTY = '',
   ACCOUNT_EXIST = 'Username already exits',
